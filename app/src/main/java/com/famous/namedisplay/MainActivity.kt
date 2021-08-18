40db752cfb53b0c04fdd4ad11509ed160a836f1a
Success0452 <olatundesuccess54@gmail.com>
2021-08-18 08:02:50 +0100

    update

--- app/src/main/java/com/famous/namedisplay/MainActivity.kt
+++ app/src/main/java/com/famous/namedisplay/MainActivity.kt
@@ -7,15 +7,15 @@ import com.famous.namedisplay.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-       val input = binding.input.text.trim()
         binding.displayBtn.setOnClickListener {
-            binding.displayTxt.text = input.toString()
+            val input = binding.input.text
+            binding.displayTxt.text = input
         }
 
     }
